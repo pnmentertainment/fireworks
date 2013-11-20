c7291c0eaf44a3bb2cb329918644e15a6668a6bb
Jon Lam <jon@noventalearning.com>
2013-11-20 08:26:52 -0700

    Archiving Nov 19
    Archiving build

---
+++ 2013-Nov-19/Assets/NGUIParseTest.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading.Tasks;
+using Parse;
+
+public class NGUIParseTest : MonoBehaviour
+{
+	public UILabel user;
+	public UILabel password;
+	public UILabel emailReset;
+	public UILabel usernameRegister;
+	public UILabel emailRegister;
+	public UILabel passwordRegister;
+	public UILabel repassRegister;
+	public UIPlayAnimation other; //to try and use UIPlayAnimation on other gameobject to play the animation
+	
+	public GameObject levelLock;
+	public GameObject loginPanel;
+	public GameObject registrationPanel;
+	public GameObject pwrecoveryPanel;
+	
+	private bool bLogin=false;
+	public static string message = "";
+	
+	static string staticUser;
+	
+	void Update()
+	{
+		if (bLogin)
+		{
+			LoginPreferenceSet ();
+			bLogin=false;
+		}
+	}
+	
+	void LoginPreferenceSet()
+	{
+		PlayerPrefs.SetInt("UserLoggedIn", 1);
+		levelLock.SetActive (false);
+		loginPanel.SetActive (false);
+		message = "Took lock off";
+	//	loginPanel.animation.Stop ("loginpanelup");
+	//	loginPanel.animation.Play ("loginsuccess");
+	//	other.
+		Debug.Log(loginPanel.animation.isPlaying);
+	}
+	
+	void PasswordReset() // If the password reset button is clicked in the PW reset panel, it should call this to reset the PW with Parse.
+	{
+		Task requestPasswordTask = ParseUser.RequestPasswordResetAsync(emailReset.text);
+		Debug.LogError ("Password reset called");
+	}
+	
+	void Registration()
+	{
+		Debug.LogError("Registration has been called");
+		 if (usernameRegister.text == "Your full name" || emailRegister.text == "Your email" || repassRegister.text == "Enter password again" || usernameRegister.text == "" || emailRegister.text == "" || passwordRegister.text == "" || repassRegister.text == "")
+		{
+			message = "Please enter all the fields";
+			Debug.LogError ("" +message);
+		}
+            else
+            {
+                if (passwordRegister.text == repassRegister.text)
+                {
+					Debug.LogError("Passwords Matched");
+					var userCurrent = new ParseUser(){
+						Username = usernameRegister.text,
+						Email = emailRegister.text,
+						Password = passwordRegister.text
+					};
+//					Debug.LogError ("" +userCurrent);
+					Task signUpTask = userCurrent.SignUpAsync();
+					message = "Sign up successful. Check your email for verification!";
+                }
+                else
+		{
+                   message = "Your Password does not match";
+            }
+	}
+	}
+	
+	void Authentication()
+	{
+//		print ("user"+user+"pass"+password);
+//		staticUser = user.;
+		ParseUser.LogInAsync(user.text, password.text).ContinueWith(t =>
+		{
+			if (t.IsFaulted || t.IsCanceled)
+			{
+				message = "error logging in \n";
+				Debug.LogError(""+t.Exception);
+			}
+			else
+			{
+				bLogin=true;
+//				PlayerPrefs.SetInt("UserLoggedIn", 1);
+				print ("alrity");
+			}
+		})
+		;
+	}
+	
+	// Update is called once per frame
+
+}
---
+++ 2013-Nov-19/Assets/errorLabel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class errorLabel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		UILabel lbl = GetComponent<UILabel>();
+		lbl.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UILabel lbl = GetComponent<UILabel>();
+		lbl.text = "" +NGUIParseTest.message;
+	}
+}
