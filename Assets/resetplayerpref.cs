e681dabe15a7fd72651141bdf7f614fc4f49f1f3
CaptainSputnik <sean.dunkley@gmail.com>
2013-11-24 17:24:27 -0700

    -Changed clouds in scene 1 to simple planes

---
+++ Assets/resetplayerpref.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class resetplayerpref : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	PlayerPrefs.SetInt("UserLoggedIn", 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
